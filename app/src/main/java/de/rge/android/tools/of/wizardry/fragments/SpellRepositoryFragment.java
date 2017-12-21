90e35c0d2297f9b22ccc512a3bfd8b09341c2be8
Roman Gupta <Roman_Gupta@gmx.net>
2017-12-21 21:33:43 +0100

    implement a spell repository
    added a listview on the spell repository tab with dummy spells

--- app/src/main/java/de/rge/android/tools/of/wizardry/fragments/SpellRepositoryFragment.java
+++ app/src/main/java/de/rge/android/tools/of/wizardry/fragments/SpellRepositoryFragment.java
@@ -1,14 +1,20 @@
 package de.rge.android.tools.of.wizardry.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import de.rge.toolsofwizardry.R;
 
 public class SpellRepositoryFragment extends Fragment {
+
+    private static final String[] DUMMY_SPELLS = {"Burning Hands", "Scorching Ray", "Fireball"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -17,5 +23,19 @@ public class SpellRepositoryFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        initListView(view);
+    }
+
+    private void initListView(View view) {
+        ListView spellRepository = view.findViewById(R.id.lvwSpellRepository);
+        spellRepository.setAdapter(createListViewAdapter());
+    }
+
+    @NonNull
+    private ArrayAdapter<String> createListViewAdapter() {
+        return new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_selectable_list_item,
+                DUMMY_SPELLS);
     }
 }
